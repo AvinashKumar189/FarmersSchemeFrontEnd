8eab5e463551ce499af64018af5640cccd7fd315
Avinash <avinash.birkh@gmail.com>
2020-09-27 19:40:46 +0530

    more routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { FarmerOrBidderComponent } from './farmer-or-bidder/farmer-or-bidder.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'contact-us', component: ContactUsComponent},
+  {path: 'forgot-password', component:ForgotPasswordComponent},
+  {path: 'farmer-or-bidder', component:FarmerOrBidderComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
