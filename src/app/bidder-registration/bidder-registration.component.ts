97308b39e5bddf6e41afa784167308c02792bfdc
RishaliSinha <rishali510@gmail.com>
2020-10-01 00:33:34 +0530

    bidder registration validated

--- src/app/bidder-registration/bidder-registration.component.ts
+++ src/app/bidder-registration/bidder-registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {ValidatorFn} from '@angular/forms';
 
 @Component({
   selector: 'app-bidder-registration',
@@ -8,13 +9,34 @@ import { Router } from '@angular/router';
 })
 export class BidderRegistrationComponent implements OnInit {
 
+  fileContent: any;
+
   constructor(private router: Router) { }
+
+  bidder: Bidder = new  Bidder();
 
   ngOnInit(): void {
   }
 
   goRegisterSuccess(){
-    this.router.navigate(['/register-success']);
+    alert(JSON.stringify(this.bidder));
+    //this.router.navigate(['/register-success']);
   }
+
+}
+export class Bidder{
+  fullName: string;
+  contactNo: number;
+  emailId: string;
+  address: string;
+  city: string;
+  state: string;
+  pinCode : number;
+  accountNo : number;
+  ifsc: string;
+  aadhar: string;
+  pan: string;
+  traderLicense: string;
+  password: string;
 
 }
