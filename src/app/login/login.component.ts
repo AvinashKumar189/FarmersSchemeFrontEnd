ae83537d25f493a1491d58c10617e247a8a8be2a
Avinash <avinash.birkh@gmail.com>
2020-10-01 15:54:10 +0530

    login component form validation done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,10 +9,19 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   constructor(private router: Router) { }
-  
+  user: User= new User();
+
+  checkLogin(){
+    // alert(JSON.stringify(this.user));
+     console.log(this.user);
+   }
 
   ngOnInit(): void {
   }
 
 
+}
+export class User{
+  userName: string;
+  password: string;
 }
