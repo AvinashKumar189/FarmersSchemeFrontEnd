fdaa0ff2679a6697e099ae43624fcec6bdbb8ea5
arnab39478 <arnab39478@gmail.com>
2020-10-03 22:53:27 +0530

    after-login-bidder created

--- src/app/after-login-bidder/after-login-bidder.component.ts
+++ src/app/after-login-bidder/after-login-bidder.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { BidderService } from './../bidder.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,11 +9,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AfterLoginBidderComponent implements OnInit {
   toggle: boolean = false;
+  cropList: CropDetails[];
+  id: number;
+  name: string;
+  currentBid:number;
+  liveBid:number;
+  basePrice:number;
+  bidInProcess: BidInProcess=new BidInProcess();
 
-  constructor() {}
+  constructor(private bidderService: BidderService, private router: Router) {}
 
-  ngOnInit(): void {}
-  placeBid() {
-    this.toggle = !this.toggle;
+  ngOnInit(): void {
+    this.bidderService.fetchAllCrops().subscribe( data =>{
+      alert(JSON.stringify(data));
+      this.cropList=data.cropsForSale;
+    }
+
+    )
   }
+
+  displayCropForBidding(id: number, name: string, price: number){
+    this.id=id;
+    this.name=name;
+    this.basePrice=price;
+    this.toggle = !this.toggle;
+    this.bidderService.fetchCropDetails(this.id).subscribe( data =>{
+      alert(JSON.stringify(data));
+      this.currentBid=data.amount;
+    }
+
+    )
+  }
+
+  placeBidForCrop() {
+    if(this.liveBid>(this.currentBid+100)&& this.liveBid>this.basePrice){
+      this.bidInProcess.sellRequestId=this.id;
+      this.bidInProcess.amount=this.liveBid;
+      this.bidInProcess.bidderId=12;
+      //this.bidInProcess.bidderId=Number(sessionStorage.getItem('UserId')); 
+      this.bidderService.submitBidForCrop(this.bidInProcess).subscribe( data =>{
+        alert(JSON.stringify(data));
+        if(data.status)
+          alert(data.statusMessage);
+        else 
+          alert(data.statusMessage);
+      }
+
+      )
+    }
+    else
+      alert("Invalid Bid amount!");
+  }
+}
+
+export class CropDetails{
+  cropId:number;
+  cropName:string;
+  cropType:string;
+  basePrice:number;
+  currentBid: number;
+}
+
+export class BidInProcess{
+
+  sellRequestId: number;
+  bidderId:number;
+  amount:number;
+  
 }
--- src/app/bidder.service.ts
+++ src/app/bidder.service.ts
@@ -3,6 +3,7 @@ import { Bidder } from './bidder-registration/bidder-registration.component';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import{ Observable } from 'rxjs';
+import { BidInProcess } from './after-login-bidder/after-login-bidder.component';
 
 
 @Injectable({
@@ -13,8 +14,24 @@ export class BidderService {
   constructor(private http: HttpClient) { }
 
     goRegisterSuccess(bidder: Bidder) : Observable<any> {
-      let url= 'http://localhost:8080/bidder-register';
+      let url= 'http://localhost:8181/bidder-register';
       return this.http.post(url,bidder);
+    }
+
+    fetchAllCrops(): Observable<any>{
+      let url = 'http://localhost:8181/view-crop-list';
+      return this.http.get<any>(url);
+    }
+
+    fetchCropDetails(id:number): Observable<any>{
+      let url = 'http://localhost:8181/view-crop-details?sellRequestId='+id;
+      return this.http.get<any>(url);
+    }
+
+    submitBidForCrop(bid:BidInProcess): Observable<any>{
+      let url = 'http://localhost:8181/place-bid';
+      return this.http.post<any>(url, bid);
+
     }
 
 
