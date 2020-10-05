08255e9a6874466afb966ab76781baa13290db86
Avinash <avinash.birkh@gmail.com>
2020-10-05 15:31:04 +0530

    admin dashboard created

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
-
+  
+  farmer : Farmer = new Farmer();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+export class Farmer {
+  fullName : string;
+  contactNo : string;
+  emailId : string;
+  address : string;
+  city : string;
+  state : string;
+  pinCode : number;
+  landArea : string;
+  landAddress : string;
+  landPincode : number;
+  accountNo : number;
+  ifsc : string;
+  aadhar : string;
+  pan : string;
+  certificate : string;
+  password : string;
+  confirmPassword : string;
 }
