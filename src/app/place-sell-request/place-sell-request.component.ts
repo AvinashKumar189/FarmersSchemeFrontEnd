9f0d896c41daeb5a4d20392f93cc0c275c9edee1
RishaliSinha <rishali510@gmail.com>
2020-10-07 11:45:13 +0530

    place-sell-request tested

--- src/app/place-sell-request/place-sell-request.component.ts
+++ src/app/place-sell-request/place-sell-request.component.ts
@@ -11,26 +11,30 @@ export class PlaceSellRequestComponent implements OnInit {
   sellRequest: SellRequest = new SellRequest();
   data: any;
 
+
   constructor(
     private router: Router,
     private sellRequestService: SellRequestService
   ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+  }
 
   goSellRequest() {
-    alert(JSON.stringify(this.sellRequest));
-    //console.log(this.sellRequest);
+    //alert(JSON.stringify(this.sellRequest));
+    this.sellRequest.farmerId = Number(sessionStorage.getItem("userId"));
+    console.log(this.sellRequest);
     this.sellRequestService
       .goSellRequest(this.sellRequest)
       .subscribe(response => {
         this.data = response;
       });
-    //this.router.navigate(['/sell-request-success']);
+    this.router.navigate(['/sell-request-success']);
   }
 }
 
 export class SellRequest {
+  farmerId: number;
   cropType: string;
   cropName: string;
   fertilizerType: string;
